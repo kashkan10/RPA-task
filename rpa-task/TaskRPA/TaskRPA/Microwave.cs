09eb54c7ab291e55b07301efc4acbb3ae3719955
kashkan10 <a.kashkan10@gmail.com>
2020-03-13 15:56:44 +0300

    Add files via upload

---
+++ rpa-task/TaskRPA/TaskRPA/Microwave.cs
@@ -0,0 +1,9 @@
+﻿namespace TaskRPA
+{
+    public class Microwave
+    {
+        public string Title { get; set; }
+        public string Price { get; set; }
+        public string Href { get; set; }
+    }
+}
